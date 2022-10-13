2e1530d0acac3a0d603e0b9d3f5139dcf6263d45
luciano <lnsiqueira@gmail.com>
2022-10-13 14:24:17 +0200

    added documentation

--- flutter_sample_mvp/lib/air_quality/repositories/air_quality_repository.dart
+++ flutter_sample_mvp/lib/air_quality/repositories/air_quality_repository.dart
@@ -2,10 +2,12 @@ import 'package:dio/dio.dart';
 
 import '../models/air_quality_model.dart';
 
+/// Class that accesses UK government API to return various information about the city of London [].
 class AirQualityRepository {
   final dio = Dio();
   final url = 'https://api.tfl.gov.uk/AirQuality/';
 
+  ///# Returns a type AirQualityModel object with all API data - AirQuality
   Future<AirQualityModel> fetchAirQuality() async {
     final response = await dio.get(url);
     final airQuality = AirQualityModel.fromJson(response.data);
